4e723fb9390e3b3909bef164c6a77a0a6d406587
celinechege <chegeceline18@gmail.com>
2023-03-17 12:27:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+let num1=31,num2=2
+let multiply =num1*num2
+console.log (multiply);
+
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+let num3=22,num4=6
+let mod =num3%num4
+console.log(mod)
+
+let max= Math.max(2,3,18,20,13)
+console.log(max)
